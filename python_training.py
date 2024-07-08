dafb0ea24ed977abe6c677834d19675b5d9c6475
Cmort12 <108856224+Cmort12@users.noreply.github.com>
2024-07-08 06:19:06 +0000

    updates

--- python_training.py
+++ python_training.py
@@ -1,12 +1,11 @@
-
-#### Chapter 1 and Chapter 2 ####
+# Chapter 1 and Chapter 2 #
 print("Hello World!")
 print("Goodbye World!")
 4*4
 10/2 +3
 name = "Craig"
 print("Hello" , name)
-##### Chapter 3 ####
+# Chapter 3 #
 age = 36
 print(age)
 print(type(age))
@@ -27,8 +26,26 @@ print(type(cookies))
 45000/5
 0.2+0.5
 0.2/0.3
-
-#### Chapter 4 ####
+# Chapter 4 #
 4==2*2
 name = "Annyce"
 name== "Frank"
+5==4 #does 5 equal 4?#
+5!=4 #does 5 not equal 4?#
+5>4 #is 5 greater than 4#
+5<4 #is 5 less than 4#
+age=15
+age_to_drive = 15
+age == age_to_drive
+# An if else statement #
+if 5>6:
+    print("Yes, 5 is greater than 6.")
+else: 
+    print("No, 5 is not greater than 6") 
+    print("Everyone knows that")
+
+answer = input("Hi, What is your favourite food?")
+food = "pizza"
+if answer == print(food):
+    print("Yep! So amazing!")
+else: print("Yuck! That's not it!")
