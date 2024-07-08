3f486d0d7212f18440a354492381ded6f8e2d7fb
Cmort12 <108856224+Cmort12@users.noreply.github.com>
2024-07-08 14:47:37 +1000

    Add files via upload

---
+++ python_training.py
@@ -0,0 +1,30 @@
+
+#### Chapter 1 and Chapter 2 ####
+print("Hello World!")
+print("Goodbye World!")
+4*4
+10/2 +3
+name = "Craig"
+print("Hello" , name)
+##### Chapter 3 ####
+age = 36
+print(age)
+print(type(age))
+email = "john.doe@example.com" 
+print(email)
+print(type(email))
+cookies = "Sugar"
+print(cookies)
+cookies = 1 
+print(cookies)
+print(type(cookies))
+2+3
+2*3
+2**3 #### power ###
+3/2 #### division###
+3//2 #### division without remainders ####
+5%3 #### remainders from division ####
+45000/5
+0.2+0.5
+0.2/0.3
+
---
+++ python_training2.py
@@ -0,0 +1,9 @@
+print("Hi!")
+name = input("What is your name?")
+
+print("It's nice to meet you,", name)
+answer = input("Are you enjoying the course?")
+
+If answer== "Yes"
+    print("That's good to hear!")
+else: print("Oh no! that makes me sad!")
